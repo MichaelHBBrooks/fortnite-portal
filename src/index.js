0363c120bcb5dccc34bb1b833705418b501165be
MichaelHBBrooks <Michael.H.B.Brooks@gmail.com>
2018-01-19 22:28:22 -0500

    Deleted unused files. Created Full container.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from "./registerServiceWorker";
+import Full from "./containers/Full";
+
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route path="/" name="Home" component={Full} />
+        </Switch>
+    </Router>,
+    document.getElementById("root")
+);
 registerServiceWorker();
